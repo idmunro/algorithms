580077bb299406e4ac6cde437c8c9d8b8cc53aa6
Ian Munro <ianmunro501@gmail.com>
2022-05-26 15:30:11 -0600

    Implemented merge sort

--- cpp/src/sorting/MergeSorter.h
+++ cpp/src/sorting/MergeSorter.h
@@ -3,16 +3,102 @@
 
 #include "Sorter.h"
 #include <iostream>
+#include <typeinfo>
 
+
+// typedef unsigned long long size_t;
 
 template<typename T>
-class MergeSorter : public Sorter<T> {
+class MergeSorter : public Sorter<T> 
+{
 
     public:
-        MergeSorter() {
-            std::cout << "hello";
+        MergeSorter() 
+        {
+            std::cout << "hello" << std::endl;
         }
-        T& sort(T &data) const {
+
+        T& sort(T &data, size_t start, size_t end, Method m = Method::RECURSIVE) const 
+        {
+            if(m == Method::RECURSIVE)
+            {
+                sort_recursive(data, start, end);
+            }
+            else if(m == Method::ITERATIVE)
+            {
+                sort_iterative(data, start, end);
+            }
+            return data;
+        }
+
+    private:
+
+        T& sort_recursive(T &data, size_t start, size_t end) const
+        {
+            if(start == end) return data;
+            size_t mid = start + ((end - start) / 2);
+            sort_recursive(data, start, mid);
+            sort_recursive(data, mid+1, end);
+            sort_recursive(data, start, mid, mid+1, end);
+            return data;
+        }
+        T& sort_recursive(T &data, size_t s1, size_t e1, size_t s2, size_t e2) const 
+        {
+            if (s1 == s2) 
+            {
+                return data;
+            }
+            else
+            {
+                for(int i=s1; i<=e1;) 
+                {
+                    for(int j=s2; j<=e2 && i<=e1;)
+                    {
+                        if(data[i] <= data[j])
+                        {
+                            ++j;
+                        }
+                        else if(data[i] > data[j])
+                        {
+                            auto temp = data[i];
+                            data[i] = data[j];
+                            data[j] = temp;
+                            ++i;
+                        }
+                    }
+                    ++i;
+                }
+            }
+            return data;
+        }
+
+        T& sort_iterative(T &data, size_t start, size_t end) const 
+        {
+            int split = 2;
+            int data_size = (end - start) + 1;
+            for(int split = 2; split < data_size; split *= 2)
+            {
+                int split_start = start;
+                for(int i=start; i < end; i=split_start+split)
+                {
+                    split_start = i;
+                    int mid = (i + split) / 2;
+                    for(int j = i; j < split_start+split && j <= end;)
+                    {
+                        if(data[i] <= data[j])
+                        {
+                            ++j;
+                        }
+                        else if(data[i] > data[j])
+                        {
+                            auto temp = data[i];
+                            data[i] = data[j];
+                            data[j] = temp;
+                            ++i;
+                        }
+                    }
+                }
+            }
             return data;
         }
 
--- cpp/src/sorting/Sorter.h
+++ cpp/src/sorting/Sorter.h
@@ -3,12 +3,20 @@
 
 #include <array>
 
+typedef unsigned long long size_t;
+
+enum class Method {
+    RECURSIVE,
+    ITERATIVE
+};
+
 template<typename T> 
 class Sorter {
     
     public:
+
     // .sort() Return the original data structure in sorted order
-        T& sort(T &data) const;
+        T& sort(T &data, size_t start, size_t end, const Method m) const;
 
     // .sort_copy() Return a new data structure of copied valus in sorted order
         // virtual T& sort_copy(const T& data) const;
