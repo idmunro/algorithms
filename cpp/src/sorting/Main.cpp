927aa8d374c5b0fc1ef6a6294005e9dc3f6866d7
Ian Munro <ianmunro501@gmail.com>
2022-05-26 16:05:00 -0600

    planning

--- cpp/src/sorting/Main.cpp
+++ cpp/src/sorting/Main.cpp
@@ -1,6 +1,26 @@
 #include "MergeSorter.h"
+#include "../speedtest/SpeedTest.h"
+
 #include <vector>
 
 int main() {
+    int j[6] = {3, 4, 1, 5, 7, 6};
+    std::vector<int> k{3, 4, 1, 5, 7, 6};
     MergeSorter<std::vector<int>> m;
+    MergeSorter<int[6]> n;
+
+    SpeedTest<> st;
+    st.add_test([&]() mutable {m.sort(k, 0, 5, Method::ITERATIVE);});
+    st.add_test([&]() mutable {n.sort(j, 0, 5, Method::ITERATIVE);});
+    st.run_all();
+    st.print_report();
+    
+    for(auto i: k)
+        std::cout << i << ", ";
+    std::cout << std::endl;
+    
+    for(auto i: j)
+        std::cout << i << ", ";
+    std::cout << std::endl;
+
 };
